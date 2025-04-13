409076bd3a8d06f1f4fe23b12a6b81a2ba04b293
kirubakaran <72642390+kirubakaran2@users.noreply.github.com>
2025-04-13 15:07:27 +0530

    committhevercelfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/': {
+        target: 'https://ragulconstructions-backend.onrender.com',
+        bypass: (req) => req.headers.accept?.includes('html') ? '/' : null
+      }
+    }
   },
 });

a4f8e26dd6cf0f97349bc2ce42b285e9eedba349
kirubakaran <72642390+kirubakaran2@users.noreply.github.com>
2025-04-13 14:20:14 +0530

    committeurl

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -26,7 +26,7 @@ const Projects = () => {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/projects');
+        const response = await fetch('https://ragulconstructions-backend.onrender.com/api/projects');
         const data = await response.json();
         setProjects(data);
         setLoading(false);
